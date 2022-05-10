7f34283500fd7423a3af30977dc4f1fdc2b9ddc6
ASUS <Yehor Kholodiuk>
2022-05-10 19:06:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Switch from "./Routers/Switch";
+import {Routes, Route, Link, Router} from "react-router-dom";
+import Links from "./Routers/Links";
+
+import ClientTable from "./Clients/ClientTable";
+import Orders from "./Orders/Orders";
+import Services from "./Services/Services";
+import 'bootstrap/dist/css/bootstrap.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+   <h1>Accounting</h1>
+
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link className="nav-link active" aria-current="page"to="/clients" >Clients</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/orders" >Orders</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/services" >Services</Link>
+            </li>
+
+        </ul>
+            <nav>
+                <Link to ="/clients">Clients</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/services">Services</Link>
+
+            </nav>
+
+            <Routes>
+                <Route path="clients" element={<ClientTable/>} />
+                <Route path="orders" element={<Orders/>} />
+                <Route path="services" element={<Services/>} />
+
+            </Routes>
+
+
+      <Links/>
+      <Switch/>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Clients/ClientTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClientTable = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ClientTable;
